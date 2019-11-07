fd40becaab65602e39b052fa0ea4b533e22dbfb2
Mike Abebe <mikiastilahun@gmail.com>
2019-11-07 14:36:35 +0300

    bla bla bla

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>this is mike from ethiopia with love</p>
+        <p>this is mike from ethiopia with love and respect</p>
         <a
           className="App-link"
           href="https://reactjs.org"
